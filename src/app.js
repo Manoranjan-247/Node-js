61cd13c4bf01f997cce3b4a67d46bd75357570ad
Manoranjan-247 <manoranjan.parida@watsoo.com>
2025-07-08 12:47:36 +0530

    init - database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const connectDB = require("./config/database");
-
+const User = require('./models/user')
 
 const app = express();
+
+app.post("/signup", async (req, res) =>{
+    const user = new User({
+        firstName: "Virat",
+        lastName: "Kohli",
+        emailId:"virat@gmail.com",
+        password:"Virat@123"
+    })
+
+    try {
+        await user.save();
+
+    res.send("user added successfully!!!!!!");
+    } catch (error) {
+        res.status(400).send("Error saving the user : " + error.message)
+    }
+    
+})
 
 connectDB()
     .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://manoranjanparida:dE0IMuJ30VYLysKv@cluster0.v05nwot.mongodb.net/'); // connect to the cluster
+    await mongoose.connect('mongodb+srv://manoranjanparida:dE0IMuJ30VYLysKv@cluster0.v05nwot.mongodb.net/devTinder'); // connect to the cluster
 };
 
 module.exports = connectDB;
